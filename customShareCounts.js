3ba68ae250c3c0d6129227a04cb3bba24e00bccd
David ODey <dodey@oreillyauto.com>
2016-05-08 00:42:45 -0700

    re-wrote to make it more object based

--- customShareCounts.js
+++ customShareCounts.js
@@ -1,136 +1,141 @@
-/*
- The MIT License (MIT)
-
- Copyright (c) <year> <copyright holders>
-
- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
-
- Author: David O'Dey
- Website: http://www.davidodey.com
- Version: 1.1
-
+/*!
+ *  Custom share counts with totals!
+ *  Version: 1.0.0
+ *  Author: David O'Dey - www.davidodey.com
+ *  License: MIT http://en.wikipedia.org/wiki/MIT_License or GPLv2 http://en.wikipedia.org/wiki/GNU_General_Public_License
  */
+
 (function ($) {
 	'use strict';
 
 	$.fn.customShareCount = function( options ) {
 		var settings = $.extend({}, $.fn.customShareCount.defaults, options);
-		function loadCounts(url, jsonUrl, callback) {
-			$.ajax({
-				url: url,
-				cache: true,
-				type: 'POST',
-				dataType: 'jsonp',
-				data: {
-					url: jsonUrl
-				},
-				success: function (data){
-					callback(data);
-				}
-			});
-		}
-		function formatK(num) {
-			return num > 999 ? (num/1000).toFixed(1) + 'k' : num;
-		}
-		
-		function showCount (target, value) {
-			if (settings.showCounts === true) {
-				target.text(formatK(value));
-				if (value === undefined) {
-					target.text(0);
-				}
-			} else {
-				target.remove();
-			}
-		}
-		
+
 		this.on('click', 'a', function () {
 			window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');
 			return false;
 		});
-		
+
 		return this.each(function(){
-			var $twitterCount = $(this).find('.twitter-count'),
+			var $countUrl = $(this).attr('data-url'),
+				$countTitle = $(this).attr('data-title'),
+				$twitterHash = $(this).attr('data-hash'),
 				$facebookCount = $(this).find('.facebook-count'),
 				$linkedinCount = $(this).find('.linkedin-count'),
+				$twitterCount = $(this).find('.twitter-count'),
 				$googleCount = $(this).find('.google-count'),
 				$totalCount = $(this).find('.total-count'),
-				$thisUrl = $(this).attr('data-url'),
-				$thisTitle = $(this).attr('data-title'),
-				$twitterHash = $(this).attr('data-hash'),
-				twitterLoadUrl = 'http://twitter.com/intent/tweet?text='+ $thisTitle + ' ' + $thisUrl + '&amp;via=' + settings.twitterUsername + ' %23' + $twitterHash + '&amp;source=webclient',
-				twitterJsonUrl = 'http://opensharecount.com/count.json?url=' + $thisUrl,
-				facebookLoadUrl = 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent($thisUrl),
-				facebookJsonUrl = 'http://graph.facebook.com/?id=' + $thisUrl,
-				linkedInLoadUrl = 'http://www.linkedin.com/shareArticle?mini=true&url=' + encodeURIComponent($thisUrl),
-				linkedInJsonUrl = 'https://www.linkedin.com/countserv/count/share?url=' + $thisUrl,
-				googlePlusLoadUrl = 'https://plus.google.com/share?url=' + encodeURIComponent($thisUrl),
-				googlePlusJsonUrl = 'https://count.donreach.com/',
-				totalCount = 0;
+				ltotalCount = 0,
+				ftotalCount = 0,
+				ttotalCount = 0,
+				gtotalCount = 0,
 
-			if (settings.twitter === true) {
-				$(this).find('a.twitterBtn').attr('href', twitterLoadUrl);
-				loadCounts(twitterJsonUrl, $thisUrl, function (data) {
-					showCount($twitterCount, data.count);
-					if (settings.showTotal === true && isNaN(data.count) === false) {
-						totalCount += data.count;
-						$totalCount.text(formatK(totalCount));
-
+			network = {
+				placeHref: function (target, url) {
+					target.attr('href', url);
+				},
+				loadJson: function (url, jsonUrl, item, target, callback) {
+					if (settings.showCounts === true) {
+						$.ajax({
+							url: jsonUrl,
+							cache: true,
+							type: 'POST',
+							dataType: 'jsonp',
+							data: {
+								url: url
+							},
+							success: function (data){
+								target.text(network.convertK(data[item]));
+								callback(data);
+							}
+						});
 					}
-				});
-			}
+				},
+				total: function (){
+					if (settings.showTotal === true ){
+						$totalCount.text(network.convertK(ltotalCount + ftotalCount + ttotalCount + gtotalCount));
+					}
+				},
+				convertK: function (num) {
+					return num > 999 ? (num/1000).toFixed(1) + 'k' : num;
+				},
+				facebook: {
+					jsonUrl: 'http://graph.facebook.com/?id=' + $countUrl,
+					linkUrl: 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent($countUrl),
+					linkTarget: $(this).find('a.facebookBtn'),
+					load: function () {
+						network.placeHref(this.linkTarget, this.linkUrl);
+						network.loadJson($countUrl, this.jsonUrl, 'shares', $facebookCount, function(data) {
+							ftotalCount = data.shares;
+							network.total();
+						});
+					}
+				},
+				linkedin: {
+					jsonUrl: 'https://www.linkedin.com/countserv/count/share?url=' + $countUrl,
+					linkUrl: 'http://www.linkedin.com/shareArticle?mini=true&url=' + encodeURIComponent($countUrl),
+					linkTarget: $(this).find('a.linkedinBtn'),
+					load: function () {
+						network.placeHref(this.linkTarget, this.linkUrl);
+						network.loadJson($countUrl, this.jsonUrl, 'count', $linkedinCount, function(data) {
+							ltotalCount = data.count;
+							network.total();
+						});
+					}
+				},
+				twitter: {
+					jsonUrl: 'http://opensharecount.com/count.json?url=' + $countUrl,
+					linkUrl:  'http://twitter.com/intent/tweet?text='+ $countTitle + ' '
+								+ $countUrl + '&amp;via=' + settings.twitterUsername + ' %23'
+								+ $twitterHash + '&amp;source=webclient',
+					linkTarget: $(this).find('a.twitterBtn'),
+					load: function () {
+						network.placeHref(this.linkTarget, this.linkUrl);
+						network.loadJson($countUrl, this.jsonUrl, 'count', $twitterCount, function(data) {
+							ttotalCount = data.count;
+							network.total();
+						});
+					}
+				},
+				google: {
+					jsonUrl: 'https://count.donreach.com/',
+					linkUrl: 'https://plus.google.com/share?url=' + encodeURIComponent($countUrl),
+					linkTarget: $(this).find('a.googleBtn'),
+					load: function () {
+						network.placeHref(this.linkTarget, this.linkUrl);
+						network.loadJson($countUrl, this.jsonUrl, 'shares', $googleCount, function(data) {
+							$googleCount.text(network.convertK(data.shares.google));
+							gtotalCount = data.shares.google;
+							network.total();
+						});
+					}
+				}
+			};
 			if (settings.facebook === true) {
-				$(this).find('a.facebookBtn').attr('href', facebookLoadUrl);
-				loadCounts(facebookJsonUrl, $thisUrl, function (data) {
-					showCount($facebookCount, data.shares);
-					if (settings.showTotal === true && isNaN(data.shares) === false) {
-						totalCount += data.shares;
-						$totalCount.text(formatK(totalCount));
-
-					}
-				});
+				network.facebook.load();
 			}
 			if (settings.linkedin === true) {
-				$(this).find('a.linkedinBtn').attr('href', linkedInLoadUrl);
-				loadCounts(linkedInJsonUrl, $thisUrl, function (data) {
-					showCount($linkedinCount, data.count);
-					if (settings.showTotal === true && isNaN(data.count) === false) {
-						totalCount += data.count;
-						$totalCount.text(formatK(totalCount));
-
-					}
-				});
+				network.linkedin.load();
+			}
+			if (settings.twitter === true) {
+				network.twitter.load();
 			}
 			if (settings.google === true) {
-				$(this).find('a.googleBtn').attr('href', googlePlusLoadUrl);
-				loadCounts(googlePlusJsonUrl, $thisUrl, function (data) {
-					showCount($googleCount, data.shares.google);
-					if (settings.showTotal === true && isNaN(data.shares.google) === false){
-						totalCount += data.shares.google;
-						$totalCount.text(formatK(totalCount));
-
-					}
-				});
+				network.google.load();
 			}
+
 		});
 	};
 
 	$.fn.customShareCount.defaults = {
-		
 		// set true or false to toggle the counts & API calls
 		twitter: true,
 		facebook: true,
 		linkedin: true,
 		google: true,
-		
 		twitterUsername: '',
-		showCounts: true,
-		showTotal: true,
-		blogRoll: true
+		showCounts: false,
+		showTotal: true
 	};
 
 }(jQuery));
